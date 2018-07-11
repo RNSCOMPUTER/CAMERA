87a738344684b2429350c28be7fa55a50400c916
RNSCOMPUTER <41129504+RNSCOMPUTER@users.noreply.github.com>
2018-07-11 17:47:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const notifyServiceSid = process.env.NOTIFY_SERVICE_SID;
+const accountSid = AC3bde980418244db77dfe1e7bb992eed3;
+const authToken = 88fcfbef539a9e745195fa1f913abfc6;
+const notifyServiceSid = IS7d840ce0bcd3eab4d9c0e0b7fb999240;
 
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(AC3bde980418244db77dfe1e7bb992eed3, 88fcfbef539a9e745195fa1f913abfc6);
 
-client.notify.services(notifyServiceSid)
+client.notify.services(IS7d840ce0bcd3eab4d9c0e0b7fb999240)
   .notifications.create({
     toBinding: JSON.stringify([
-      binding_type: 'sms', address: '** First phone number here **',
-      binding_type: 'sms', address: '** Second phone number here **',
-      // This also works for iOS, Android, and FB Messenger.
-      // You can mix and match binding_type in the toBinding.
+      binding_type: 'sms', address: '6477073954',
+      binding_type: 'sms', address: '647-863-9232',
     ]),
-    body: 'You just sent your first message with the Passthrough API!'
+    body: 'Test message!'
   })
-  .then(notification => console.log(notification.sid))
+  .then(notification => console.log(IS7d840ce0bcd3eab4d9c0e0b7fb999240))
   .catch(error => console.log(error));
